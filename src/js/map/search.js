becee2a278092ebcabd19a707b2971aafa68d393
sanxof <sanxof@iMac2.broadband>
2020-03-25 16:15:50 +0000

    start

---
+++ src/js/map/search.js
@@ -0,0 +1,22 @@
+class Search {
+    constructor(map, layersData) {
+        this.mapClass = map;
+        this.checkboxStates = {};
+        this.mapConfig = map.mapConfig;
+        this.layersData = layersData;
+        this.container = map.container;
+        this.search = null;
+    }
+    
+    init() {
+        this.search = this.mapConfig.search;
+        this.createMarkup();
+    }
+
+    createMarkup(){
+        
+    }
+
+}
+
+export default Search;
