aef790f957c354ba0e80a86ed75118629a43bfa1
sanxof <sanxof@iMac2.broadband>
2020-03-26 16:32:19 +0000

    search class

--- src/js/map/search.js
+++ src/js/map/search.js
@@ -1,21 +1,33 @@
 class Search {
-    constructor(map, layer) {
-        this.map = map;
-        this.mapConfig = map.mapConfig;
-        //this.layersData = layersData;
-        this.layer = layer;
-        this.container = map.container;
+    constructor(mapClass) {
+        this.map = mapClass.map;
+        this.mapConfig = mapClass.mapConfig;
         this.search = null;
+        this.searchLayer = null;
     }
     
     init() {
         this.search = this.mapConfig.search;
         //this.createMarkup();
-        L.Control.Search({layer: this.layer}).addTo(this.map);
+        this.searchLayer = new L.LayerGroup([]);
     }
 
     createMarkup(){
-        
+        const controlSearch = new L.Control.Search({
+            position:'topleft',		
+            layer: this.searchLayer,
+            propertyName: this.search.searchField,
+            textPlaceholder: this.search.searchBoxText,
+            collapsed: false,
+            initial: false,
+            //zoom: 18,
+            marker: false
+        });
+        controlSearch.on('search:locationfound', function(e) {           
+            if(e.layer._popup)
+              e.layer.openPopup();       
+        });
+        this.map.addControl(controlSearch);
     }
 
 }
