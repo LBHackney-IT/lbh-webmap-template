8b1c0ec99784f3bfc212fed3a4b5abaae3b2b826
sanxof <sanxof@iMac2.broadband>
2020-03-30 00:01:51 +0100

    add object to map if not there

--- src/js/map/search.js
+++ src/js/map/search.js
@@ -39,10 +39,16 @@ class Search {
             marker: false
             //class: 'govuk-input  lbh-input'
         });
-        controlSearch.on('search:locationfound', function(e) {           
-            if(e.layer._popup)
-              e.layer.openPopup();       
-        });
+        // controlSearch.on('search:locationfound', function(e) {           
+        //     if(e.layer._popup)
+        //       e.layer.openPopup(); 
+        //       e.layer.addTo(this.map);      
+        // });
+        controlSearch.on('search:locationfound', (e) => {           
+          if(e.layer._popup)
+            e.layer.addTo(this.map);
+            e.layer.openPopup();      
+      });
         this.map.addControl(controlSearch);
     }
 
