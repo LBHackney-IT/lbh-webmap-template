6dac0e9d9d0e4acee4de420ecf569945935189a5
sanxof <sanxof@iMac2.broadband>
2020-03-30 15:02:33 +0100

    clear map before displaying search result

--- src/js/map/search.js
+++ src/js/map/search.js
@@ -16,12 +16,11 @@ class Search {
     createMarkup(){
         let html = `<details class="govuk-details lbh-details" data-module="govuk-details">
     <summary class="govuk-details__summary">
-      <span class="govuk-details__summary-text">
-        Map search
-      </span>
+      <span class="govuk-details__summary-text">`
+      +this.search.searchSectionTitle
+      +`</span>
     </summary>
-
-  <div id="searchdiv"></div>
+  <div class="govuk-details__text" id="searchdiv"></div>
   </details>
     </section>`;
 
@@ -32,11 +31,13 @@ class Search {
             container: 'searchdiv',
             layer: this.searchLayer,
             propertyName: this.search.searchField,
-            textPlaceholder: this.search.searchBoxText,
+            textPlaceholder: this.search.searchPlaceholderText,
             collapsed: false,
             initial: false,
             //zoom: 18,
-            marker: false
+            marker: false,
+            textErr: this.search.notFoundText,
+            autoCollapseTime: 4000
             //class: 'govuk-input  lbh-input'
         });
         // controlSearch.on('search:locationfound', function(e) {           
@@ -46,8 +47,9 @@ class Search {
         // });
         controlSearch.on('search:locationfound', (e) => {           
           if(e.layer._popup)
-            e.layer.addTo(this.map);
-            e.layer.openPopup();      
+          this.mapClass.clear();  
+          e.layer.addTo(this.map);
+          e.layer.openPopup();      
       });
         this.map.addControl(controlSearch);
     }
