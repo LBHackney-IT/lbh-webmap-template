5dbd55873b59f9639f5f26de9199955db1deb7e6
sanxof <sanxof@iMac2.broadband>
2020-05-07 11:12:02 +0100

    tests

--- src/js/map/list-view.js
+++ src/js/map/list-view.js
@@ -15,19 +15,37 @@ class List {
   }
 
   createMarkup() {
-    let html = `<div class="govuk-accordion listview lbh-accordion" data-module="govuk-accordion" id="listview" data-attribute="value">
+    let html = `<div class="govuk-accordion lbh-accordion" data-module="govuk-accordion" id="default-example" data-attribute="value">
     <div class="govuk-accordion__section ">
       <div class="govuk-accordion__section-header">
         <h5 class="govuk-accordion__section-heading">
-          <span class="govuk-accordion__section-button" id="listview-heading-1">`
-          + this.layersData+
-          `</span>
+          <span class="govuk-accordion__section-button" id="default-example-heading-1">
+            Section A
+          </span>
         </h5>
       </div>
-      <div id="listview-content-1" class="govuk-accordion__section-content" aria-labelledby="listview-heading-1">
+      <div id="default-example-content-1" class="govuk-accordion__section-content" aria-labelledby="default-example-heading-1">
         <ul class="lbh-list lbh-list--bullet">
-<li>Example item 1</li>
-</ul></div></div>`;
+  <li>Example item 1</li>
+  </ul>
+  
+      </div>
+    </div>
+    <div class="govuk-accordion__section ">
+      <div class="govuk-accordion__section-header">
+        <h5 class="govuk-accordion__section-heading">
+          <span class="govuk-accordion__section-button" id="default-example-heading-2">
+            Section B
+          </span>
+        </h5>
+      </div>
+      <div id="default-example-content-2" class="govuk-accordion__section-content" aria-labelledby="default-example-heading-2">
+        <ul class="lbh-list lbh-list--bullet">
+  <li>Example item 2</li>
+  </ul> 
+      </div>
+    </div>
+  </div>`;
     
     this.mapClass.addMarkupToMap(html, "listview", "listview");
 
