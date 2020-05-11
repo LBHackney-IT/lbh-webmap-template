1adfe2191bf3b6ea184c2dcdc421f2b6c05b2643
sanxof <sanxof@iMac2.broadband>
2020-05-11 22:42:25 +0100

    option to show icons or not in list view

--- src/js/map/list-view.js
+++ src/js/map/list-view.js
@@ -13,7 +13,6 @@ class List {
   init() {
     this.list = this.mapConfig.list;
     this.layersData.sort((a, b) => (a.layer.options.sortOrder > b.layer.options.sortOrder) ? 1 : -1);
-    console.log(this.layersData);
     this.createMarkup();
   }
 
@@ -22,16 +21,28 @@ class List {
     let html = `<div class="listview-container"><h3>${this.list.sectionHeader}</h3>`;
     html += `<div class="govuk-accordion lbh-accordion" data-module="govuk-accordion" data-attribute="value">`;
     for (var layerData of this.layersData){
-      console.log(layerData)
       if (layerData.configLayer.listView){
-        html += `<div class="govuk-accordion__section ">
-        <div class="govuk-accordion__section-header">
-          <h5 class="govuk-accordion__section-heading">
+        if (this.list.showIcons){
+          html += `<div class="govuk-accordion__section ">
+            <div class="govuk-accordion__section-header">
+            <h5 class="govuk-accordion__section-heading">
             <span class="govuk-accordion__section-button">
             ${layerData.configLayer.title} &nbsp <i class="fas fa-${layerData.configLayer.pointStyle.icon}" style="color:${MARKER_COLORS[layerData.configLayer.pointStyle.markerColor]}"></i>
             </span>
-          </h5>
-        </div>`;
+            </h5>
+          </div>`;
+        }
+        else{
+          html += `<div class="govuk-accordion__section ">
+            <div class="govuk-accordion__section-header">
+            <h5 class="govuk-accordion__section-heading">
+            <span class="govuk-accordion__section-button">
+            ${layerData.configLayer.title}
+            </span>
+            </h5>
+          </div>`;
+        }
+        
         for (var feat of layerData.layer.getLayers()){
           //for (var feature of layerData.data.features){
           // html += `<div id="default-example-content-1" class="govuk-accordion__section-content" aria-labelledby="default-example-heading-1">
