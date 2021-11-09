c4e980d4c898203714769bf82bf082cac22b99da
Marta <marta.villalobos@hackney.gov.uk>
2021-11-09 17:45:36 +0000

    remove duplicates in the drill down and update link in the planning constraints map

--- src/js/map/drill-down.js
+++ src/js/map/drill-down.js
@@ -23,26 +23,26 @@ class DrillDown {
    
     this.map.eachLayer(layer => {
       if (layer.feature){
-        console.log(layer);
+        //console.log(layer);
         //If the feature is a polygon...
         if (layer.feature.geometry.type == 'Polygon'){
-          console.log("polygon");
-          console.log(intersectingFeatures.includes(layer));
+          //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
+          if (!intersectingFeatures.includes(layer)){
           let turfPoly = turf.polygon(layer.feature.geometry.coordinates);
-          //Check if the point is within the polygon. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
-          if (turf.booleanPointInPolygon(turfClickPoint, turfPoly)){
-            intersectingFeatures.push(layer);
-            if (globalPopUp != ''){
-              globalPopUp += '<br/><hr><br/>';
+            //Check if the point is within the polygon. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
+            if (turf.booleanPointInPolygon(turfClickPoint, turfPoly)){
+              intersectingFeatures.push(layer);
+              if (globalPopUp != ''){
+                globalPopUp += '<br/><hr><br/>';
+              }
+              globalPopUp += layer.getPopup().getContent();
             }
-            globalPopUp += layer.getPopup().getContent();
           }
         }
         //If the feature is a multipolygon...
         else if (layer.feature.geometry.type == 'MultiPolygon'){
-          console.log("multipolygon");
-          console.log(intersectingFeatures.includes(layer));
           layer.feature.geometry.coordinates.forEach(part => {
+            //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
             if (!intersectingFeatures.includes(layer)){
               let turfPoly = turf.polygon(part);
               if (turf.booleanPointInPolygon(turfClickPoint, turfPoly)){
@@ -57,61 +57,47 @@ class DrillDown {
         }
         //If the feature is a point...
         else if (layer.feature.geometry.type == 'Point'){
-          console.log("point");
-          console.log(intersectingFeatures.includes(layer));
-          let turfPoint = turf.point(layer.feature.geometry.coordinates);
-          let turfClickPointBuffer = turf.buffer(turfClickPoint, 0.01, {units: 'kilometers'});         
-          //Check if the feature point is whithin the clicked location buffer. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
-          if (turf.booleanPointInPolygon(turfPoint,turfClickPointBuffer)){
-            intersectingFeatures.push(layer);
-            if (globalPopUp != ''){
-              globalPopUp += '<br/><hr><br/>';
+          //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
+          if (!intersectingFeatures.includes(layer)){
+            let turfPoint = turf.point(layer.feature.geometry.coordinates);
+            let turfClickPointBuffer = turf.buffer(turfClickPoint, 0.01, {units: 'kilometers'});         
+            //Check if the feature point is whithin the clicked location buffer. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
+              if (turf.booleanPointInPolygon(turfPoint,turfClickPointBuffer)){
+                intersectingFeatures.push(layer);
+                if (globalPopUp != ''){
+                  globalPopUp += '<br/><hr><br/>';
+                }
+                globalPopUp += layer.getPopup().getContent();
+              }       
             }
-            globalPopUp += layer.getPopup().getContent();
-          }       
         }
          //If the feature is a multipoint
          else if (layer.feature.geometry.type == 'MultiPoint'){
-          console.log("multipoint");
-          console.log(intersectingFeatures.includes(layer));
-          let turfClickPointBuffer = turf.buffer(turfClickPoint, 0.01, {units: 'kilometers'}); 
-          layer.feature.geometry.coordinates.forEach(point => {
-            let turfPoint = turf.point(point);
-           //Check if the feature point is whithin the clicked location buffer. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
-          if (turf.booleanPointInPolygon(turfPoint,turfClickPointBuffer)){
-            intersectingFeatures.push(layer);
-            if (globalPopUp != ''){
-              globalPopUp += '<br/><hr><br/>';
-            }
-            globalPopUp += layer.getPopup().getContent();
-          }   
-          })               
+          //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
+          if (!intersectingFeatures.includes(layer)){
+              let turfClickPointBuffer = turf.buffer(turfClickPoint, 0.01, {units: 'kilometers'}); 
+              layer.feature.geometry.coordinates.forEach(point => {
+                let turfPoint = turf.point(point);
+              //Check if the feature point is whithin the clicked location buffer. If it is, we push the feature into the intersectingFeatures array and create get the popUp content
+              if (turf.booleanPointInPolygon(turfPoint,turfClickPointBuffer)){
+                intersectingFeatures.push(layer);
+                if (globalPopUp != ''){
+                  globalPopUp += '<br/><hr><br/>';
+                }
+                globalPopUp += layer.getPopup().getContent();
+              }   
+            })   
+          }            
         }
         //If the feature is a line...
         else if(layer.feature.geometry.type == 'LineString'){
-          console.log("linestring");
-          console.log(intersectingFeatures.includes(layer));
-          //Create a turf line from the feature, create the bounding box and covert the bounding box into a turf polygon. 
-          let turfLine = turf.lineString(layer.feature.geometry.coordinates);
-          //Create a buffer around the clickLatLong point (the clicked location)
-          let clickLatLonBuffer = turf.buffer(turfClickPoint, 0.02, {units: 'kilometers'});
-          //Check if both polygons intersect. If they do, we push the feature into the intersectingFeatures array and create get the popUp content
-          if (turf.lineIntersect(clickLatLonBuffer, turfLine).features.length > 0) {
-            intersectingFeatures.push(layer);
-            if (globalPopUp != ''){
-              globalPopUp += '<br/><hr><br/>';
-            }
-            globalPopUp += layer.getPopup().getContent();
-          }
-        }
-        //If the feature is a multiline...
-        else if (layer.feature.geometry.type == 'MultiLineString'){
-          console.log("multilinestring");
-          console.log(intersectingFeatures.includes(layer));
-          let clickLatLonBuffer = turf.buffer(turfClickPoint, 0.02, {units: 'kilometers'});
-          layer.feature.geometry.coordinates.forEach(part => {
-            let turfLine = turf.lineString(part);
-            console.log(intersectingFeatures.includes(layer));
+          //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
+          if (!intersectingFeatures.includes(layer)){
+            //Create a turf line from the feature, create the bounding box and covert the bounding box into a turf polygon. 
+            let turfLine = turf.lineString(layer.feature.geometry.coordinates);
+            //Create a buffer around the clickLatLong point (the clicked location)
+            let clickLatLonBuffer = turf.buffer(turfClickPoint, 0.02, {units: 'kilometers'});
+            //Check if both polygons intersect. If they do, we push the feature into the intersectingFeatures array and create get the popUp content
             if (turf.lineIntersect(clickLatLonBuffer, turfLine).features.length > 0) {
               intersectingFeatures.push(layer);
               if (globalPopUp != ''){
@@ -119,16 +105,29 @@ class DrillDown {
               }
               globalPopUp += layer.getPopup().getContent();
             }
-          })
-        }   
-                 
+          }
+        }
+        //If the feature is a multiline...
+        else if (layer.feature.geometry.type == 'MultiLineString'){
+          //check if the feature is already in the intersecting features array. If is not included, then continue with the checking
+          if (!intersectingFeatures.includes(layer)){
+            let clickLatLonBuffer = turf.buffer(turfClickPoint, 0.02, {units: 'kilometers'});
+            layer.feature.geometry.coordinates.forEach(part => {
+              let turfLine = turf.lineString(part);
+              if (turf.lineIntersect(clickLatLonBuffer, turfLine).features.length > 0) {
+                intersectingFeatures.push(layer);
+                if (globalPopUp != ''){
+                  globalPopUp += '<br/><hr><br/>';
+                }
+                globalPopUp += layer.getPopup().getContent();
+              }
+           })
+          }
+        }            
       }
-    
-
     });
 
     if (intersectingFeatures.length > 0){
-      console.log(intersectingFeatures);
       this.map.openPopup(globalPopUp, e.latlng, {
         offset: L.point(0, -24),
         maxHeight: 300
